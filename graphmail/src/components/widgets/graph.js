c7661163761562a0d214eb723b8a5a1382f6b657
Tony Chau <tonychau923@gmail.com>
2021-06-25 00:52:14 +1000

    Update graph.js

--- graphmail/src/components/widgets/graph.js
+++ graphmail/src/components/widgets/graph.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 import Chart from 'react-apexcharts';
+// const {ExportManager, ExportConfig} = require('fusionexport-node-client');
 
 export default class Graph extends Component {
     constructor(props){
         super(props);
         this.componentDidUpdate = this.componentDidUpdate.bind(this);
         this.componentDidMount = this.componentDidMount.bind(this);
-        this.getDerivedStateFromProps = this.getDerivedStateFromProps.bind(this);
     }
     state = {
         options: {},
         series:[],
-        type: this.props.type,
         height: this.props.height,
-        width: this.props.width
+        width: this.props.width,
+        type: this.props.type
     }
+    // https://apexcharts.com/docs/export-to-pdf-in-node-js/
 
     componentDidMount() {
         var xname, yname;
@@ -33,64 +34,78 @@ export default class Graph extends Component {
             y_data.push(eval(`item.${yname}`));
         });
 
-        var xdata =  [
+        var data =  [
             {
                 name: xname,
                 data: x_data
             }
         ];
         var options = {
-            xaxis:{
-                categories: y_data
-            }
+                chart:{
+                    height: this.props.height,
+                    width: this.props.width,
+                    type: this.props.type
+                },
+                xaxis:{
+                    categories: y_data
+                }
         }
-        this.setState({series: xdata, options});
-    }
-
-    static getDerivedStateFromProps(props, state){
-        var xname, yname;
-        props.headings.forEach((item) =>{
-            if (props.xHeadingKey == item.id){
-                xname = item.name;
-            }
-            if (props.yHeadingKey == item.id){
-                yname = item.name;
-            }
-        });
-        var x_data = [];
-        var y_data = [];
-        props.excelJson.forEach((item) =>{ 
-            x_data.push(eval(`item.${xname}`));
-            y_data.push(eval(`item.${yname}`));
-        });
-
-        this.setState({type: props.type});
+        this.setState({series: data, options});
     }
 
     componentDidUpdate(prevprops, prevstate){
-        return;
-        var xname, yname;
-        this.props.headings.forEach((item) =>{
-            if (this.props.xHeadingKey == item.id){
-                xname = item.name;
-            }
-            if (this.props.yHeadingKey == item.id){
-                yname = item.name;
-            }
-        });
-        var x_data = [];
-        var y_data = [];
-        this.props.excelJson.forEach((item) =>{ 
-            x_data.push(eval(`item.${xname}`));
-            y_data.push(eval(`item.${yname}`));
-        });
-
-        if (this.props.type !== prevprops.type){
-            this.setState({type: this.props.type});
-            console.log(prevstate.type, this.state.type);
+        if (prevprops.xHeadingKey !== this.props.xHeadingKey || prevprops.yHeadingKey !== this.props.yHeadingKey){
+            var xname, yname;
+            this.props.headings.forEach((item) =>{
+                if (this.props.xHeadingKey == item.id){
+                    xname = item.name;
+                }
+                if (this.props.yHeadingKey == item.id){
+                    yname = item.name;
+                }
+            });
+            var x_data = [];
+            var y_data = [];
+            this.props.excelJson.forEach((item) =>{ 
+                x_data.push(eval(`item.${xname}`));
+                y_data.push(eval(`item.${yname}`));
+            });
+            var data =  [
+                {
+                    name: xname,
+                    data: x_data
+                }
+            ];
+            var options = prevstate.options;
+            options.xaxis.categories = y_data;
+            // var options = {
+            //     chart:{
+            //         height: this.props.height,
+            //         width: this.props.width,
+            //         type: this.props.type
+            //     },
+            //     xaxis:{
+            //         categories: y_data
+            //     }
+            // }
+            this.setState({options, series: data});
         }
 
+        if (this.props.type !== prevprops.type){
+            console.log(prevstate, prevprops);
+            var options = prevstate.option;
+            options.chart.type = this.props.type;
 
+            this.setState({type: this.props.type, options});
+        }
+
+        if (this.props.height !== prevprops.height){
+            this.setState({height: this.props.height});
+        }
+
+        if(this.props.width !== prevprops.width){
+            this.setState({width: this.props.width});
+        }
     }
 
 
@@ -109,9 +124,9 @@ export default class Graph extends Component {
                 <p style={titlestyle}>{this.props.title}</p>
                 <Chart options = {this.state.options}
                 series = {this.state.series}
-                type={this.state.type}
-                width={this.state.width}
-                height={this.state.height}/>
+                height = {this.state.height}
+                width = {this.state.width}
+                type = {this.state.type}/>
                 <p>This graph was generated by <a href="https://apexcharts.com/" target="_blank">ApexCharts</a></p>
             </div>
                 
